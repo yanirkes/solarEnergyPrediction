9f05fd74bdf357b0278e7bdecabe8cbcc6726af4
yanirkes <yanirkes@gmail.com>
2021-12-24 19:44:07 +0200

    improve code

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from timeit import default_timer as timer
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, train_test_split, PredefinedSplit
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 from dask_ml.wrappers import ParallelPostFit
 
 
@@ -24,20 +24,9 @@ scalar = StandardScaler().fit(x_val)
 x_val_st = scalar.transform(x_val)
 
 # Predefined Val Split
-validation_idx = np.repeat(-1, y_train.shape)
-validation_idx[np.random.choice(validation_idx.shape[0],
-       int(round(.2*validation_idx.shape[0])), replace = False)] = 0
-
-# Now, create a list which contains a single tuple of two elements,
-# which are arrays containing the indices for the development and
-# validation sets, respectively.
-validation_split = list(PredefinedSplit(validation_idx).split())
-
-# sanity check
-print(len(validation_split[0][0]))
-print(len(validation_split[0][0])/float(validation_idx.shape[0]))
-print(validation_idx.shape[0] == y_train.shape[0])
-print(set(validation_split[0][0]).intersection(set(validation_split[0][1])))
+validation_indices = np.zeros(y_train.shape[0])
+validation_indices[:round(3/4*y_train.shape[0])] = -1
+validation_split = PredefinedSplit(validation_indices)
 
 #### Defaulted Models
 # knn
@@ -70,7 +59,7 @@ svrPipiLineDef.fit(x_train,y_train)
 
 ########################################## DASK ######################################
 #### Non Defaulted Defaulted Models
-# knn (Evri using dask we were able to run the model in 20-40 % less, check ParallelPostFit)
+# knn (Evri, using dask we were able to run the model in 20-40 % less, check ParallelPostFit)
 # knn
 print('\nKNN\n')
 scalar = StandardScaler()
@@ -82,7 +71,8 @@ knnPipiLine = Pipeline([
 gdSearchKnn = GridSearchCV(knnPipiLine
                            , {'model__estimator__n_neighbors':[1,2,3,4,5,6,7,8]}
                            , cv = validation_split
-                           ,refit=False
+                           , refit=False
+                           , error_score='raise'
                            , scoring='neg_mean_absolute_error')
 
 start = timer()
@@ -102,7 +92,6 @@ gdSearchDt = RandomizedSearchCV(dtPipiLine
                                         , 'model__estimator__min_samples_split': [2, 5, 10, 20, 50]
                                         , 'model__estimator__max_features': ['auto','sqrt']
                                         , 'model__estimator__min_samples_leaf': [2,4,10]
-                                        , 'model__estimator__criterion': ['MAE']
                                       }
                                     , cv = validation_split
                                     , refit=False
@@ -125,9 +114,10 @@ svrPipiLine = Pipeline([
     ])
 gdSearchSvr = RandomizedSearchCV(svrPipiLine
                                     , {'model__estimator__C':[1,2,3,4]
-                                        ,'model__estimator__kernel': ['linear', 'rbf']
+                                        ,'model__estimator__kernel': ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']
                                       }
-                                    , cv = validation_split,refit=False
+                                    , cv = validation_split
+                                    , refit=False
                                     , scoring='neg_mean_absolute_error'
                                 )
 
@@ -138,20 +128,24 @@ print(f"time for SVR: {train_patched:.2f} s")
 print('SVR best hyperparameters: ', gdSearchSvr.best_params_)
 
 ######### Re-fiting the models with the best params ##########
-knn = ParallelPostFit(KNeighborsRegressor(n_neighbors=8))
+knn = ParallelPostFit(KNeighborsRegressor(n_neighbors=gdSearchKnn.best_params_['model__estimator__n_neighbors']))
 knnPipiLine = Pipeline([
     ('standartization',scalar),
     ('model', knn)
     ])
 knnPipiLine.fit(x_train,y_train)
 
-DT = ParallelPostFit(DecisionTreeRegressor(min_samples_split=10, min_samples_leaf = 10, max_features= 'auto', max_depth = 10))
+DT = ParallelPostFit(DecisionTreeRegressor(min_samples_split = gdSearchDt.best_params_['model__estimator__min_samples_split']
+                                           , min_samples_leaf = gdSearchDt.best_params_['model__estimator__min_samples_leaf']
+                                           , max_features= gdSearchDt.best_params_['model__estimator__max_features']
+                                           , max_depth = gdSearchDt.best_params_['model__estimator__max_depth']))
 DtPipiLine = Pipeline([
     ('model', DT)
     ])
 DtPipiLine.fit(x_train,y_train)
 
-svr = ParallelPostFit(SVR(kernel = 'linear', C = 3))
+svr = ParallelPostFit(SVR(kernel = gdSearchSvr.best_params_['model__estimator__kernel']
+                          , C = gdSearchSvr.best_params_['model__estimator__C']))
 SvrPipiLine = Pipeline([
     ('standartization',scalar),
     ('model', svr)
@@ -160,22 +154,26 @@ SvrPipiLine.fit(x_train,y_train)
 
 ######## Models Comparison #####################
 print('\nDeafult result')
-print('KNN Default MSE: ', round(mean_absolute_error(y_val, knnPipiLineDef.predict(x_val_st)), 4))
-print('DT Default MSE: ', round(mean_absolute_error(y_val, dtPipiLineDef.predict(x_val)), 4))
-print('SVR Default MSE: ', round(mean_absolute_error(y_val, svrPipiLineDef.predict(x_val_st)), 4))
+print('KNN Default MAE: ', round(mean_absolute_error(y_val, knnPipiLineDef.predict(x_val_st)), 4))
+print('DT Default MAE: ', round(mean_absolute_error(y_val, dtPipiLineDef.predict(x_val)), 4))
+print('SVR Default MAE: ', round(mean_absolute_error(y_val, svrPipiLineDef.predict(x_val_st)), 4))
 print('\nHyperparameter optimization modeled result')
-print('KNN MSE: ', round(mean_absolute_error(y_val, knnPipiLine.predict(x_val_st)), 4))
-print('DT MSE: ', round(mean_absolute_error(y_val, DtPipiLine.predict(x_val)), 4))
-print('SVR MSE: ', round(mean_absolute_error(y_val, SvrPipiLine.predict(x_val_st)), 4))
+print('KNN MAE: ', round(mean_absolute_error(y_val, knnPipiLine.predict(x_val_st)), 4))
+print('DT MAE: ', round(mean_absolute_error(y_val, DtPipiLine.predict(x_val)), 4))
+print('SVR MAE: ', round(mean_absolute_error(y_val, SvrPipiLine.predict(x_val_st)), 4))
 
 ######## DT regressor the best model
 x = train.iloc[:, 0:75]
 y = train.iloc[:, -1]
-final_DT = ParallelPostFit(DecisionTreeRegressor(min_samples_split=10, max_features= 'sqrt', max_depth = 5))
+final_DT = ParallelPostFit(DecisionTreeRegressor(min_samples_split = gdSearchDt.best_params_['model__estimator__min_samples_split']
+                                           , min_samples_leaf = gdSearchDt.best_params_['model__estimator__min_samples_leaf']
+                                           , max_features= gdSearchDt.best_params_['model__estimator__max_features']
+                                           , max_depth = gdSearchDt.best_params_['model__estimator__max_depth']))
 DtFinalPipiLine = Pipeline([
     ('model', final_DT)
     ])
 DtFinalPipiLine.fit(x,y)
 x_test = test.iloc[:, 0:75]
 y_test = test.iloc[:, -1]
-print('DT MSE: ', round(mean_absolute_error(y_test, DtFinalPipiLine.predict(x_test)), 4))
+print('DT MAE: ', round(mean_absolute_error(y_test, DtFinalPipiLine.predict(x_test)), 4))
+print('DT MAPE: ', round(mean_absolute_percentage_error(y_test, DtFinalPipiLine.predict(x_test)), 4))
